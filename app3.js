a5fdb289d342eb9011cbd10daa82f2438c8c37a3
shaquibkhan <connectshaquib@gmail.com>
2017-04-29 14:07:17 +0530

    commit
    commit

---
+++ app3.js
@@ -0,0 +1,21 @@
+//    ************ Array of objects..****************
+var Employee =[
+				{'name':'shaquib ahmad khan','age':23,'salary':65000,'city':'new-delhi','state':'northern-india','pincode':'110025'},
+				{'name':'shoaib akhtar','age':23,'salary':65000,'city':'new-delhi','state':'northern-india','pincode':'110021'},
+				{'name':'shray kumar','age':26,'salary':45000,'city':'lucknow','state':'uttar-pradesh','pincode':'110185'},
+				{'name':'virat kohli','age':28,'salary':165000,'city':'bangalore','state':'karnataka','pincode':'123009'},
+				{'name':'irfan pathan','age':33,'salary':95000,'city':'barodra','state':'gujarat','pincode':'113490'},
+				];
+				//for printing all the keys of one employee altogether..
+				//console.log(Employee[0]);
+
+				//for printing keys of obj individually..
+				console.log(Employee[0].name);
+				console.log(Employee[0].age);
+				console.log(Employee[0].salary);
+				console.log(Employee[0].city);
+				console.log(Employee[0].state);
+				console.log(Employee[0].pincode);
+				
+				
+				
